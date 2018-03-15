b797647d08d4cd329de406278860d2727f46fa32
Greg Stimson <stimsong@yahoo.com>
2018-03-15 20:42:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
 ###### Reverse Each Word Lab using .each #####
-def reverse_each_word(sentence)
-  sentence_array = sentence.split
-  reversed_words = []
-
-  sentence_array.each do |reversed|
-     reversed_words << reversed.reverse
-  end
-  reversed_words.join(" ")
-end
+#def reverse_each_word(sentence)
+#  sentence_array = sentence.split
+#  reversed_words = []
+#
+#  sentence_array.each do |reversed|
+#     reversed_words << reversed.reverse
+#  end
+#  reversed_words.join(" ")
+#end
 
 ###### Reverse Each Word Lab using .collect #####
-def collect(sentence)
+def reverse_each_word(sentence)
   sentence.collect(&:reverse!)
 end
 
